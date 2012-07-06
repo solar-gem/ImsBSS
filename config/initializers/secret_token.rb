b2bc097c6e45febd55dd830dc23f2912de6438d5
Sergey Sintyalev <sintyalev@mail.ru>
2012-07-06 09:25:29 +0400

    Создана начальная авторизация (без учёта разделения ролей пользователей). Создана основная html форма

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImsBSS::Application.config.secret_token = 'b2975b657dbc268471d8b4704748729cf928c989eb77ce09e0f45574301517287a186f6d536cc1a1bb62d05f6d49abb452c3a6c01fd8d76ed21cac92ce7e3a58'
